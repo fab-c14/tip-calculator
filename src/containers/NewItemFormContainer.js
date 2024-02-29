c0f8868aec1ea215a855212142c7b6239f84660a
Faisal Bhat <plesim18@gmail.com>
2024-02-29 07:39:45 +0000

    Remove Items

--- src/containers/NewItemFormContainer.js
+++ src/containers/NewItemFormContainer.js
@@ -2,7 +2,9 @@ import { connect } from "react-redux";
 import NewItemForm from "../components/NewItemForm";
 import { addNewItem } from "../store/items/actions";
 import { bindActionCreators } from "redux";
-const mapDispatchToProps = {
-    onSubmit:(name,price)=>addNewItem(name,price)
+const mapDispatchToProps =(dispatch)=> {
+    return bindActionCreators({
+        onSubmit:(name,price)=>addNewItem(name,price)
+    },dispatch)
 }
 export const NewItemFormContainer = connect(null,mapDispatchToProps)(NewItemForm)
--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,5 +1,5 @@
 let id = 1;
-import { ITEM_ADDED } from "./actions";
+import { ITEM_ADDED, ITEM_REMOVED } from "./actions";
 export const intialItems = [
     { uuid: 1, name: 'Awsome Tofu Roast', price: 14, quantity: 1 },
     { uuid: 2, name: 'Vegan Ham Sandwich', price: 12, quantity: 1 }
@@ -9,6 +9,9 @@ export const reducer = (state=intialItems,action)=>{
     if(action.type===ITEM_ADDED){
         const item = {uuid:id++,quantity:1,...action.payload};
         return [...state,item];
+    }
+    if(action.type===ITEM_REMOVED){
+        return state.filter(item=>item.uuid!==action.payload.uuid);
     }
     return state;
 }
